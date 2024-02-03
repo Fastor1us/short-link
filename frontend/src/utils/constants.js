72077724d623fdfe10afed14e4556a7cdc549bde
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-02-03 22:36:17 +0300

    v0.1.1 @hotfix

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = 'http://localhost:3001';
+
+export const BACKEND_URL = process.env.NODE_ENV === 'production' ?
+  'https://short1fy-backend.onrender.com' : 'http://localhost:3001';
